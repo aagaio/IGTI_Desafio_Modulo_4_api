9d2b4e773d011c0de3bdcc60525afe095b7aa417
André Alexandre Gaio <aagaio@host-172-20-0-20.linwork.com.br>
2020-07-08 19:27:07 -0300

    Carga inicial no repositório particular.

---
+++ models/gradesModel.js
@@ -0,0 +1,33 @@
+
+export (default) => { 
+  const gradesSchema = mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    subject: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: Number,
+      required: true,
+      min: [0, 'Balance have to be a positive number!'],
+    },
+    lastModified: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+  });
+
+
+  const gradesModel = mongoose.model('grades', gradesSchema, 'grades');
+  return gradesModel;
+}
+
+export { gradesModel };
