1e30d8c44a5f268b01b68ea6f359ea0619fb4f9f
André Alexandre Gaio <aagaio@host-172-20-0-20.linwork.com.br>
2020-07-08 23:45:51 -0300

    adicao de prints para debug

--- models/index.js
+++ models/index.js
@@ -5,7 +5,8 @@ import dotenv from 'dotenv';
 const db = {};
 db.mongoose = mongoose;
 db.grades = gradesModel(mongoose);
-db.url = process.env.MONGODB;
+// db.url = process.env.MONGODB;
+db.url =
+  'mongodb+srv://aagaio:novenet@cluster0.ffkhj.mongodb.net/db1?retryWrites=true&w=majority';
 
-console.log('ENV ++++ ' + process.env.MONGODB);
 export { db };
